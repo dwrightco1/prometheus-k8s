281ba8f11d6342349cac988b44200862b0610bac
Dan Wright <dan.wright@platform9.com>
2019-11-22 13:39:40 +0000

    uded alertmanager and grafana to stack

--- INSTALL.sh
+++ INSTALL.sh
@@ -20,9 +20,9 @@ if [ ! -r ${kubectl} ]; then assert "missing kubectl: ${kubectl}"; fi
 if [ ! -r ${kubeconfig} ]; then assert "missing kubeconfig: ${kubeconfig}"; fi
 
 # install
-eval ${kubectl} --kubeconfig ${kubeconfig} apply -f namespace.yaml -n monitoring
-eval ${kubectl} --kubeconfig ${kubeconfig} apply -f clusterRole.yaml -n monitoring
-eval ${kubectl} --kubeconfig ${kubeconfig} apply -f config-map.yaml -n monitoring
+eval ${kubectl} --kubeconfig ${kubeconfig} apply -f prometheus-namespace.yaml -n monitoring
+eval ${kubectl} --kubeconfig ${kubeconfig} apply -f prometheus-clusterrole.yaml -n monitoring
+eval ${kubectl} --kubeconfig ${kubeconfig} apply -f prometheus-configmap.yaml -n monitoring
 eval ${kubectl} --kubeconfig ${kubeconfig} apply -f prometheus-deployment.yaml -n monitoring
 eval ${kubectl} --kubeconfig ${kubeconfig} apply -f prometheus-service.yaml -n monitoring
 
@@ -31,3 +31,8 @@ eval ${kubectl} --kubeconfig ${kubeconfig} apply -f alertManager-configmap.yaml 
 eval ${kubectl} --kubeconfig ${kubeconfig} apply -f alertTemplate-configmap.yaml -n monitoring
 eval ${kubectl} --kubeconfig ${kubeconfig} apply -f alertManager-deployment.yaml -n monitoring
 eval ${kubectl} --kubeconfig ${kubeconfig} apply -f alertManager-service.yaml -n monitoring
+
+# install grafana
+eval ${kubectl} --kubeconfig ${kubeconfig} apply -f grafana-datasource-config.yaml
+eval ${kubectl} --kubeconfig ${kubeconfig} apply -f grafana-deployment.yaml
+eval ${kubectl} --kubeconfig ${kubeconfig} apply -f grafana-service.yaml
